36019f83025d586ec8a6441234603ec77d03719a
neils94 <neils94ca@hotmail.com>
2026-05-14 15:45:23 -0700

    update hero text and other UI fixes

--- components/greeting.tsx
+++ components/greeting.tsx
@@ -13,7 +13,7 @@ export const Greeting = () => {
         transition={{ delay: 0.5 }}
         className="text-2xl font-semibold"
       >
-        Hello there!
+        Stay Buoyant!
       </motion.div>
       <motion.div
         initial={{ opacity: 0, y: 10 }}
@@ -22,7 +22,7 @@ export const Greeting = () => {
         transition={{ delay: 0.6 }}
         className="text-2xl text-zinc-500"
       >
-        How can I help you today?
+        What can we review with you?
       </motion.div>
     </div>
   );
